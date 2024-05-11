f11b974b2fbfdf990b07a944b4f1d857a47b22c0
bembelem <nikvedd@gmail.com>
2024-05-12 00:10:16 +1000

    added Descryption

--- bot.py
+++ bot.py
@@ -16,11 +16,6 @@ async def process_command_start(message: types.message):
     await bot.send_message(message.from_user.id, keyboard.startText, reply_markup=keyboard.main_keyboard)
 
 
-@disp.callback_query_handler(text=['il2'])
-async def get_link(call: types.CallbackQuery):
-    await bot.send_message(call.message.chat.id, link('Сайт ДВФУ', 'https://www.dvfu.ru/'))
-
-
 @disp.callback_query_handler(text=['il1'])
 async def get_news(call: types.CallbackQuery):
     Parser.count_n = 0
@@ -31,6 +26,11 @@ async def get_news(call: types.CallbackQuery):
                            parse_mode='HTML',
                            reply_markup=keyboard.moveBack_keyboard
                            )
+
+
+@disp.callback_query_handler(text=['il2'])
+async def get_link(call: types.CallbackQuery):
+    await bot.send_message(call.message.chat.id, link('Сайт ДВФУ', 'https://www.dvfu.ru/'))
 
 
 @disp.callback_query_handler(text=['il3'])
@@ -52,6 +52,13 @@ async def get_news(call: types.CallbackQuery):
                                     fmt.hide_link(article['link'])),
                            parse_mode='HTML',
                            reply_markup=keyboard.move_keyboard
+                           )
+
+@disp.callback_query_handler(text=['il5'])
+async def get_news(call: types.CallbackQuery):
+    await bot.send_message(call.message.chat.id,
+                           text=keyboard.descryptionText,
+                           reply_markup=keyboard.main_keyboard
                            )
 
 if __name__ == '__main__':
--- keyboard.py
+++ keyboard.py
@@ -4,16 +4,24 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 startText = 'Привет, я бот, который показывает новости с сайта Дальневосточного Федерального Университета.' \
             ' Ссылку на сайт можно получить, нажав на соответствующую кнопку'
 
-news_button = InlineKeyboardButton('Узнать последнюю новость', callback_data='il1')
-link_button = InlineKeyboardButton('Сайт портала ДВФУ', callback_data='il2', url='https://www.dvfu.ru/')
+descryptionText = 'Этот бот был создан в рамках практического занятия №11 по теме «Цифровой контент и его свойства»\n \
+Выполнили: \n \
+Ведёхин Никита Васильевич \n \
+Ча Эрнест Ирдеевич \n \
+Группа:Б9123-01.03.02-ии 2'
 
-next_button = InlineKeyboardButton('Следующая новость', callback_data='il3')
-prev_button = InlineKeyboardButton('Предыдущая новость', callback_data='il4')
+news_button = InlineKeyboardButton('Узнать последнюю новость🙄', callback_data='il1')
+link_button = InlineKeyboardButton('Сайт портала ДВФУ🤖', callback_data='il2', url='https://www.dvfu.ru/')
+descryption_button = InlineKeyboardButton('Кто выполнил💬', callback_data='il5')
+
+next_button = InlineKeyboardButton('Следующая новость👉', callback_data='il3')
+prev_button = InlineKeyboardButton('👈Предыдущая новость', callback_data='il4')
 
 main_keyboard = InlineKeyboardMarkup()
 move_keyboard = InlineKeyboardMarkup()
 moveBack_keyboard = InlineKeyboardMarkup()
 
 main_keyboard.row(link_button, news_button)
+main_keyboard.add(descryption_button)
 move_keyboard.row(prev_button, next_button)
 moveBack_keyboard.add(prev_button)
